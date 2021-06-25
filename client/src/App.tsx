f8df55b1aee323bc53916dad0f22370177b91803
Omar Baldi <omb@netcompany.com>
2021-06-25 16:58:06 +0200

    Setting up base react application with typescript for front-end side and installed concurrently for managing scripts

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1>This is the homepage front-end side of the application</h1>
+		</div>
+	);
+}
+
+export default App;
--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get('/', (_, res) => {
 });
 
 /* Server running */
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
