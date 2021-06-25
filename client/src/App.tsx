823d03c25c485c9d92d611dd6e1a38264811714d
Omar Baldi <omb@netcompany.com>
2021-06-25 17:07:11 +0200

    Installed react router dom and setting up main applications routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,43 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
 	return (
 		<div className='App'>
-			<h1>This is the homepage front-end side of the application</h1>
+			<Router>
+				{/* Navigation menu */}
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Homepage</Link>
+						</li>
+						<li>
+							<Link to='/checkout'>Checkout</Link>
+						</li>
+						<li>
+							<Link to='/admin'>Admin</Link>
+						</li>
+					</ul>
+				</nav>
+
+				{/* Render pages */}
+				<Switch>
+					<Route exact path='/'>
+						<>
+							<h1>This is the MAIN page</h1>
+						</>
+					</Route>
+					<Route path='/checkout'>
+						<>
+							<h1>This is the CHECKOUT page</h1>
+						</>
+					</Route>
+					<Route path='/admin'>
+						<>
+							<h1>This is the ADMIN page</h1>
+						</>
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
